c6900edca8904a233db31317dc0dedf415109a86
gallenvara <gallenvara@126.com>
2015-11-10 10:44:50 +0800

    Chengxiang modify RangePartitionRewriter.

---
+++ flink-examples/flink-java-examples/src/main/java/org/apache/flink/examples/java/partitioner/RangePartitionTest.java
@@ -0,0 +1,59 @@
+package org.apache.flink.examples.java.partitioner;
+
+import org.apache.flink.api.common.functions.MapPartitionFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.util.Collector;
+
+import java.util.HashSet;
+
+public class RangePartitionTest {
+
+	private static String InputFile;
+
+
+	public void testRangePartition() throws Exception {
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		DataSet<Tuple3<String, Integer, Long>> dataSet = env.readCsvFile(InputFile).fieldDelimiter(" ").lineDelimiter("\n").includeFields("111").types(String.class, Integer.class, Long.class);
+		/*DataSet<Tuple3<Integer, Long, String>> ds = env.readFile(new FileInputFormat<Tuple3<Integer, Long, String>>() {
+			@Override
+			public boolean reachedEnd() throws IOException {
+				return false;
+			}
+
+			@Override
+			public Tuple3<Integer, Long, String> nextRecord(Tuple3<Integer, Long, String> integerLongStringTuple3) throws IOException {
+				return integerLongStringTuple3;
+			}
+		}, "/testInput/input1.txt");*/
+		DataSet<Integer> output = dataSet.partitionByRange(1).mapPartition(new StringMapper());
+		output.writeAsText("hdfs://HadoopMaster:9000/gaolun/output", FileSystem.WriteMode.OVERWRITE);
+		env.setParallelism(4);
+		env.execute("range Partition");
+	}
+
+	public static void main(String[] args) throws Exception{
+		InputFile = args[0];
+		RangePartitionTest rangePartitionTest = new RangePartitionTest();
+		rangePartitionTest.testRangePartition();
+	}
+
+	public static class StringMapper implements MapPartitionFunction<Tuple3<String, Integer, Long>, Integer> {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void mapPartition(Iterable<Tuple3<String, Integer, Long>> records, Collector<Integer> out) throws Exception {
+			HashSet<Long> uniq = new HashSet<Long>();
+			int count = 0;
+			for (Tuple3<String, Integer, Long> t : records) {
+				count++;
+			}
+			out.collect(count);
+			//for (Long l : uniq) {
+			//	out.collect(l);
+			//}
+		}
+	}
+}
